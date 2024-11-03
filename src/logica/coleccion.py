87a719e9f5027678efbbfce98f5ae9cd0477b426
Laura Cristina Fonseca Gil <lc.fonsecag1@uniandes.edu.co>
2024-11-03 12:01:30 -0500

    Ajuste de camelCase a snake_case en agregar_cancion

--- src/logica/coleccion.py
+++ src/logica/coleccion.py
@@ -67,7 +67,7 @@ class Coleccion():
         return albumes
 
     def agregar_cancion(self, titulo, minutos, segundos, compositor, album_id, interpretes):
-        interpretesCancion = []
+        interpretes_cancion = []
         if len(interpretes) == 0:
             return False
         else:
@@ -82,8 +82,8 @@ class Coleccion():
                         interprete = Interprete(nombre=item["nombre"], texto_curiosidades=item["texto_curiosidades"],
                                                 cancion=nueva_cancion.id)
                         session.add(interprete)
-                        interpretesCancion.append(interprete)
-                    nueva_cancion.interpretes = interpretesCancion
+                        interpretes_cancion.append(interprete)
+                    nueva_cancion.interpretes = interpretes_cancion
                     session.add(nueva_cancion)
                     session.commit()
                     return True
@@ -95,8 +95,8 @@ class Coleccion():
                     interprete = Interprete(nombre=item["nombre"], texto_curiosidades=item["texto_curiosidades"],
                                             cancion=nueva_cancion.id)
                     session.add(interprete)
-                    interpretesCancion.append(interprete)
-                nueva_cancion.interpretes = interpretesCancion
+                    interpretes_cancion.append(interprete)
+                nueva_cancion.interpretes = interpretes_cancion
                 session.add(nueva_cancion)
                 session.commit()
                 return True
